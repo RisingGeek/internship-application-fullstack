3e3748d8c31e1fcd59c17bd053b15e9bca57f680
RisingGeek <bhavaypuri15@gmail.com>
2020-04-15 12:26:37 +0530

    Random url redirect

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let url = 'https://cfw-takehome.developers.workers.dev/api/variants';
+  let response = await fetch(url);
+  if (response.ok) {
+    let json = await response.json();
+    let urls = json.variants;
+
+    let max = urls.length;
+    let random = Math.floor(Math.random() * Math.floor(max));
+    console.log(random);
+    return Response.redirect(urls[random]);
+  }
+  else {
+    return new Response('Some error occured', {
+      headers: { 'content-type': 'text/plain' },
+    })
+  }
 }
